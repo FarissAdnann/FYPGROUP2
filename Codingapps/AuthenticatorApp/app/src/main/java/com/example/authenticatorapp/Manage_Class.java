1902cda6fb115707e227d6a54f1d73470e89ab8d
FarissAdnann <70558348+FarissAdnann@users.noreply.github.com>
2020-09-02 17:06:25 +0800

    Upload Coding Files

---
+++ Codingapps/AuthenticatorApp/app/src/main/java/com/example/authenticatorapp/Manage_Class.java
@@ -0,0 +1,14 @@
+package com.example.authenticatorapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Manage_Class extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage__class);
+    }
+}
